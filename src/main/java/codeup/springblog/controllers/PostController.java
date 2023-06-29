ede415897ded74032ace6a27f6b635055e15de69
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-06-29 13:37:15 -0600

    post controller complete

--- src/main/java/codeup/springblog/controllers/PostController.java
+++ src/main/java/codeup/springblog/controllers/PostController.java
@@ -2,6 +2,8 @@ package codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,8 +11,26 @@ public class PostController {
 
     @GetMapping("/posts")
     @ResponseBody
-    public String landingPage() {
-        return "<h1>This is the landing page!</h1>";
+    public String postsIndexPage() {
+        return "<h1>Posts Index Page</h1>";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String individualPost(@PathVariable int id) {
+        return "<h1>View an individual post</h1>";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPostForm() {
+        return "<h1>Form for creating posts</h1>";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String submitPostPage() {
+        return "<h1>Create a new post</h1>";
     }
 
 
